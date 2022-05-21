9cefa0ac2ef606fc3de2e2fce770a809e8377ad2
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-05-21 22:46:15 +0300

    validation

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,43 +4,42 @@
  * Star wars api
  * Script that prints all characters of a Star Wars movie:
  */
- const request = require('request');
- const filmId = process.argv[2];
- if (!filmId || isNaN(filmId)) {
-   process.exit(1);
- }
- const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
- 
- request(url, (error, response, body) => {
-   if (error) {
-     console.log(error);
-     return;
-   }
-   const respPromises = [];
- 
-   const json = JSON.parse(body);
-   const characters = json.characters;
- 
-   characters.forEach((character) => {
-     const url = character;
-     const promise = new Promise((resolve, reject) => {
-       request(url, (error, response, body) => {
-         if (error) {
-           reject(error);
-           return;
-         }
-         const json = JSON.parse(body);
-         resolve(json.name);
-       });
-     });
-     respPromises.push(promise);
-   });
-   Promise.all(respPromises).then((values) => {
-     values.forEach((value) => {
-       console.log(value);
-     });
-   }).catch((error) => {
-     console.log(error);
-   });
- });
- 
+const request = require('request');
+const filmId = process.argv[2];
+if (!filmId || isNaN(filmId)) {
+  process.exit(1);
+}
+const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const respPromises = [];
+
+  const json = JSON.parse(body);
+  const characters = json.characters;
+
+  characters.forEach((character) => {
+    const url = character;
+    const promise = new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        const json = JSON.parse(body);
+        resolve(json.name);
+      });
+    });
+    respPromises.push(promise);
+  });
+  Promise.all(respPromises).then((values) => {
+    values.forEach((value) => {
+      console.log(value);
+    });
+  }).catch((error) => {
+    console.log(error);
+  });
+});
