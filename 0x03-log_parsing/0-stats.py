2cd6458184f1162e85a9f0eadb45383eb9a92a1d
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-05-21 22:29:45 +0300

    changes

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,43 @@
 #!/usr/bin/python3
-"""Log Parser"""
+"""Script that reads stdin line by line and computes metrics"""
 import sys
 
 
-if __name__ == '__main__':
-    file_size = [0]
-    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-                    403: 0, 404: 0, 405: 0, 500: 0}
+def print_metrics(status_codes, file_size):
+    """function to print status codes and total file size"""
+    print("File size: {}".format(file_size))
+    for k, v in sorted(status_codes.items()):
+        if v > 0:
+            print("{}: {}".format(k, v))
 
-    def print_stats():
-        """ Print statistics """
-        print('File size: {}'.format(file_size[0]))
-        for key in sorted(status_codes.keys()):
-            if status_codes[key]:
-                print('{}: {}'.format(key, status_codes[key]))
 
-    def parse_line(line):
-        """ Checks the line for matches """
-        try:
-            line = line[:-1]
-            word = line.split(' ')
-            # File size is last parameter on stdout
-            file_size[0] += int(word[-1])
-            # Status code comes before file size
-            status_code = int(word[-2])
-            # Move through dictionary of status codes
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-        except BaseException:
-            pass
-
-    linenum = 1
-    try:
-        for line in sys.stdin:
-            parse_line(line)
-            """ print after every 10 lines """
-            if linenum % 10 == 0:
-                print_stats()
-            linenum += 1
-    except KeyboardInterrupt:
-        print_stats()
-        raise
-    print_stats()
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
+}
+counter = 1
+file_size = 0
+try:
+    for line in sys.stdin:
+        splited_line = line.strip().split()
+        if len(splited_line) < 7:
+            continue
+        file_size += int(splited_line[-1])
+        status_code = splited_line[-2]
+        if status_code in status_codes:
+            status_codes[status_code] += 1
+        if counter == 10:
+            print_metrics(status_codes, file_size)
+            counter = 1
+        else:
+            counter += 1
+    print_metrics(status_codes, file_size)
+except KeyboardInterrupt:
+    print_metrics(status_codes, file_size)
+    raise
