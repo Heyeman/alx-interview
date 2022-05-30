97a64b85a86a899f03956524bfd9e0bc0918a599
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-05-30 12:11:12 +0300

    made the function

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,13 +9,18 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    count = 0
-    coins.sort(reverse=True)
-    for coin in coins:
-        increase = int(total / coin)
-        total -= (increase * coin)
-        count += increase
+    coins.sort()
+    n = 0
+    while coins:
+        coin = coins.pop()
+        if coin <= total:
+            n += total//coin
+            total %= coin
         if total == 0:
-            return count
-
+            return n
     return -1
+
+
+print(makeChange([1, 2, 25], 37))
+
+print(makeChange([1256, 54, 48, 16, 102], 1453))
