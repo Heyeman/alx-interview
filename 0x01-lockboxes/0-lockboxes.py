574cf681578a66997c0ffe3a20f43fbe6e4f5c52
Heyeman Urgessa <59885488+Heyeman@users.noreply.github.com>
2022-11-08 15:27:25 +0300

    added some comments

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
+    '''returns true if all boxes are able to be opened'''
+
     if len(boxes) < 2:
         return True
     visited = set()
 
     def dfs(key):
+        '''is a dfs function that searches the boxes in depth'''
+
         visited.add(key)
         for box in boxes[key]:
             if 0 <= box < len(boxes) and box not in visited:
                 dfs(box)
     dfs(0)
+
     return len(visited) == len(boxes)
